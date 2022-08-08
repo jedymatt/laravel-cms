50ffdffa70763aac5dee987923e4ae31d038d13f
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-08 11:47:11 +0000

    Create inertia app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+// set data-theme attribute on html tag
+window.document.documentElement.setAttribute('data-theme', 'halloween');
+
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy)
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({ color: '#4B5563' });

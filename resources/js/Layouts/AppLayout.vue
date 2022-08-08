83daa7edc455b30af8751915223da49a2648bf0b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-08 12:49:46 +0000

    Make Navbar sticky

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -8,65 +8,67 @@ import { Link } from "@inertiajs/inertia-vue3";
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
             <div class="drawer-content">
                 <!-- Page content here -->
-                <div class="navbar bg-base-100">
-                    <div class="flex lg:flex-1">
-                        <label
-                            for="my-drawer-2"
-                            class="btn btn-ghost btn-square drawer-button lg:hidden"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                class="h-6 w-6"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                stroke-width="2"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                        </label>
-                    </div>
-                    <div class="flex-1 lg:hidden">
-                        <Link href="/" class="flex-0 btn btn-ghost px-2">
-                            <div
-                                class="font-title text-primary inline-flex text-lg transition-all duration-200 md:text-3xl"
-                            >
-                                <span class="capitalize">Apokon</span>
-                                <span class="text-base-content uppercase"
-                                    >CMS</span
-                                >
-                            </div>
-                        </Link>
-                    </div>
-                    <div class="flex-none">
-                        <div class="dropdown dropdown-end">
+                <div class="sticky top-0 z-30 bg-opacity-90 backdrop-blur">
+                    <div class="navbar bg-base-100">
+                        <div class="flex lg:flex-1">
                             <label
-                                tabindex="0"
-                                class="btn btn-ghost btn-circle avatar"
+                                for="my-drawer-2"
+                                class="btn btn-ghost btn-square drawer-button lg:hidden"
                             >
-                                <div class="w-10 rounded-full">
-                                    <img
-                                        src="https://placeimg.com/80/80/people"
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="h-6 w-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M4 6h16M4 12h16M4 18h16"
                                     />
-                                </div>
+                                </svg>
                             </label>
-                            <ul
-                                tabindex="0"
-                                class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-                            >
-                                <li>
-                                    <a class="justify-between">
-                                        Profile
-                                        <span class="badge">New</span>
-                                    </a>
-                                </li>
-                                <li><a>Settings</a></li>
-                                <li><a>Logout</a></li>
-                            </ul>
+                        </div>
+                        <div class="flex-1 lg:hidden">
+                            <Link href="/" class="flex-0 btn btn-ghost px-2">
+                                <div
+                                    class="font-title text-primary inline-flex text-lg transition-all duration-200 md:text-3xl"
+                                >
+                                    <span class="capitalize">Apokon</span>
+                                    <span class="text-base-content uppercase"
+                                        >CMS</span
+                                    >
+                                </div>
+                            </Link>
+                        </div>
+                        <div class="flex-none">
+                            <div class="dropdown dropdown-end">
+                                <label
+                                    tabindex="0"
+                                    class="btn btn-ghost btn-circle avatar"
+                                >
+                                    <div class="w-10 rounded-full">
+                                        <img
+                                            src="https://placeimg.com/80/80/people"
+                                        />
+                                    </div>
+                                </label>
+                                <ul
+                                    tabindex="0"
+                                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+                                >
+                                    <li>
+                                        <a class="justify-between">
+                                            Profile
+                                            <span class="badge">New</span>
+                                        </a>
+                                    </li>
+                                    <li><a>Settings</a></li>
+                                    <li><a>Logout</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>

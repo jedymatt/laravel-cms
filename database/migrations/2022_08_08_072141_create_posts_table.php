5753e8193e88ff7151ee164b6452fc0263aa1076
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-08 11:46:30 +0000

    Create post migration

---
+++ database/migrations/2022_08_08_072141_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->longText('content')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->dateTime('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};

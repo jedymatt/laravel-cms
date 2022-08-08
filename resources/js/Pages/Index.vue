d132f3ba3ba739ebc1bd985e0e0277d3bea2718f
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-08 12:10:39 +0000

    Add / route with empty display

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout>
+
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('posts.index');
+    return inertia('Index');
 });
 
 

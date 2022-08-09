f31d955f485f5a8b934c8b5a4a101e5013542bcb
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-09 13:18:28 +0000

    Create auth.php

---
+++ routes/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+});

bc6a4acf0bd4f30c609e4646f77fcff599e1806c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-09 01:22:15 +0000

    Paginate api posts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest()->get();
+        $posts = Post::latest()->paginate();
 
         return PostResource::collection($posts);
     }

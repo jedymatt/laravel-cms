391ce1b5f47adf00d3e54552111dec29dbf088c9
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-09 01:13:27 +0000

    Fix api posts index

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest();
+        $posts = Post::latest()->get();
 
         return PostResource::collection($posts);
     }

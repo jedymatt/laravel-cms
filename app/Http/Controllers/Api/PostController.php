b1d3b6a1ab00966e1fb0d3932fb018cd210969eb
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-09 09:04:06 +0800

    Create PostController

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+// use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest();
+
+        return PostResource::collection($posts);
+    }
+
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+}

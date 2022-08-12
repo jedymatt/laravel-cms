c765b9c427932b4533f77b80f41223a99f6771ac
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-08-12 08:28:23 +0000

    Add inertia share

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,7 +40,8 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'appName' => config('app.name'),
+            'appEnv' => config('app.env'),
         ]);
     }
 }
